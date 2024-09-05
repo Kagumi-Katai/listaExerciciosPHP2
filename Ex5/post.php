9a57e1b7b2aa5c741ddf705820c9c9cec6f0eac3
vinicius <vinicius_apple22@outlook.com>
2024-09-05 16:26:49 -0300

    adicionando as imagens ao último exercício

--- Ex5/post.php
+++ Ex5/post.php
@@ -1,65 +1,67 @@
 <?php
 
     $nome = $_POST["nome"];
-
     $jogadaPlayer = $_POST["jogada"];
-
     $jogadaCPU = random_int(1, 3);
+
+    $pedra =    "<img src='./src/pedra.png' style='width: 100px'>";
+    $papel =    "<img src='./src/papel.png' style='width: 100px'>";
+    $tesoura =  "<img src='./src/tesoura.png' style='width: 100px'>";
 
     switch ($jogadaPlayer) {
         
         case ($jogadaPlayer == 1 and $jogadaCPU == 1):
-            echo $nome . " você escolheu PEDRA!<br>";
-            echo "Seu oponenete escolheu PEDRA!<br>";
+            echo $pedra. $nome . " você escolheu PEDRA!<br>";
+            echo $pedra. " Seu oponenete escolheu PEDRA!<br>";
             echo "EMPATE!";
             break;
 
         case ($jogadaPlayer == 1 and $jogadaCPU == 2):
-            echo $nome . " você escolheu PEDRA!<br>";
-            echo "Seu oponenete escolheu PAPEL!<br>";
+            echo $pedra. $nome . " você escolheu PEDRA!<br>";
+            echo $papel. "Seu oponenete escolheu PAPEL!<br>";
             echo "DERROTA!";
             break;
 
         case ($jogadaPlayer == 1 and $jogadaCPU == 3):
-            echo $nome . " você escolheu PEDRA!<br>";
-            echo "Seu oponenete escolheu TESOURA!<br>";
+            echo $pedra. $nome . " você escolheu PEDRA!<br>";
+            echo $tesoura. "Seu oponenete escolheu TESOURA!<br>";
             echo "VITÓRIA!";
             break;
 
         case ($jogadaPlayer == 2 and $jogadaCPU == 1):
-            echo $nome . " você escolheu PAPEL!<br>";
-            echo "Seu oponenete escolheu PEDRA!<br>";
+            echo $papel. $nome . " você escolheu PAPEL!<br>";
+            echo $pedra. "Seu oponenete escolheu PEDRA!<br>";
             echo "VITÓRIA!";
             break;
 
         case ($jogadaPlayer == 2 and $jogadaCPU == 2):
-            echo $nome . " você escolheu PAPEL!<br>";
-            echo "Seu oponenete escolheu PAPEL!<br>";
+            echo $papel. $nome . " você escolheu PAPEL!<br>";
+            echo $papel. "Seu oponenete escolheu PAPEL!<br>";
             echo "EMPATE!";
             break;
     
         case ($jogadaPlayer == 2 and $jogadaCPU == 3):
-            echo $nome . " você escolheu PAPEL!<br>";
-            echo "Seu oponenete escolheu TESOURA!<br>";
+            echo $papel. $nome . " você escolheu PAPEL!<br>";
+            echo $tesoura. "Seu oponenete escolheu TESOURA!<br>";
             echo "DERROTA!";
             break;
 
         case ($jogadaPlayer == 3 and $jogadaCPU == 1):
-            echo $nome . " você escolheu TESOURA!<br>";
-            echo "Seu oponenete escolheu PEDRA!<br>";
+            echo $tesoura. $nome . " você escolheu TESOURA!<br>";
+            echo $pedra. "Seu oponenete escolheu PEDRA!<br>";
             echo "DERROTA!";
             break;
         
         case ($jogadaPlayer == 3 and $jogadaCPU == 2):
-            echo $nome . " você escolheu TESOURA!<br>";
-            echo "Seu oponenete escolheu PAPEL!<br>";
+            echo $tesoura. $nome . " você escolheu TESOURA!<br>";
+            echo $papel. "Seu oponenete escolheu PAPEL!<br>";
             echo "VITÓRIA!";
             break;
 
                 
         case ($jogadaPlayer == 3 and $jogadaCPU == 3):
-            echo $nome . " você escolheu TESOURA!<br>";
-            echo "Seu oponenete escolheu TESOURA!<br>";
+            echo $tesoura. $nome . " você escolheu TESOURA!<br>";
+            echo $tesoura. "Seu oponenete escolheu TESOURA!<br>";
             echo "EMPATE!";
             break;
     }
